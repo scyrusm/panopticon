790bd644b90cc4d8507b6f3741634bf0c18faeef
rht <rhtbot@protonmail.com>
2019-11-03 19:56:36 +0000

    test1.sh: Increase timeout time to accommodate slower computers

--- examples/test1.sh
+++ examples/test1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env expect
 
-set timeout 60
+set timeout 120
 eval spawn panopticon windowed-mean-expression-clustering --patient 43 --cell_type tumor --complexity_cutoff 1000 --figure_output example.pdf example_data/example.loom
 expect "save these clusters"
 send "n\n"
