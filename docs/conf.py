50de70bba817bbeaac1a2ba8fbadf31dd7628d30
Samuel Markson <smarkson@alum.mit.edu>
2020-12-22 16:52:02 -0500

    change to docs/requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = '0.1dev'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon',
-         'python_rtd_theme', 'sphinx_rtd_theme']
+         'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
