306225d7f91f8ed79f6aac35de39e5c403071e8e
Samuel Markson <smarkson@alum.mit.edu>
2020-12-22 16:39:40 -0500

    changes to sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../panopticon'))
+sys.path.insert(0, os.path.abspath('../../panopticon/'))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,7 +31,7 @@ release = '0.1dev'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon',
-         'python_docs_theme']
+         'python_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,12 +47,12 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_documatt_theme'
+html_theme = 'sphinx_rtd_theme'
 
-html_theme_options = {
-        "motto":"non sine sole iris"
-        }
+#html_theme_options = {
+#        "motto":"non sine sole iris"
+#        }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
