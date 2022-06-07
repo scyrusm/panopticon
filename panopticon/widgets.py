5ea6bdf987018d9d8141fb891692ccd126cbd207
Samuel Markson <smarkson@alum.mit.edu>
2022-06-07 02:09:59 -0400

    class-ification of panopticopter

--- panopticon/widgets.py
+++ panopticon/widgets.py
@@ -1,156 +1,309 @@
-def panopticopter(loom,
-        layername,
-        coord=None,
-        coord1=None,
-        coord2=None):
-    import numpy as np
-    from panopticon.utilities import import_check
-    exit_code = import_check("bqplot", 'pip install bqplot')
-    if exit_code != 0:
-        return
-    exit_code = import_check("ipywidgets", 'pip install ipywidgets')
-    if exit_code != 0:
-        return
-    if coord is not None and coord1 is None and coord2 is None:
-        coord1 = coord+' 1'
-        coord2 = coord+' 2'
-    elif coord is None and coord1 is not None and coord2 is not None:
-        pass
-    else:
-        raise Exception("Either coord must be None, xor coord1 and coord2 must be None")
-    for coordi in [coord1, coord2]:
-        if coordi not in loom.ca.keys():
-            raise Exception("{} not in loom.ca.keys()".format(coordi))
+class Panopticopter:
+    """ """
 
-    from bqplot import pyplot as plt
+    def __init__(self,
+                 loom,
+                 layername,
+                 coord1='log2(TP10k+1) PCA UMAP embedding 1',
+                 coord2='log2(TP10k+1) PCA UMAP embedding 2',
+                 n_points_to_display=1000):
+        self.loom = loom
+        self.layername = layername
 
-    from bqplot import DateScale, LinearScale, Axis, Lines, Scatter, Bars, Hist, Figure
-    from bqplot.interacts import (
-        FastIntervalSelector,
-        IndexSelector,
-        BrushIntervalSelector,
-        BrushSelector,
-        MultiSelector,
-        LassoSelector,
-        PanZoom,
-        HandDraw,
-    )
-    from traitlets import link
+        self.coord1 = coord1
+        self.coord2 = coord2
+        for coordi in [coord1, coord2]:
+            if coordi not in loom.ca.keys():
+                raise Exception("{} not in loom.ca.keys()".format(coordi))
+        self.selection1 = []
+        self.selection2 = []
+        self.subset_mask = None
+        self.fig = None
+        self.n_point_to_display = n_points_to_display
+        self.xs = None
+        self.ys = None
+        self.button_sel1 = None
+        self.button_sel2 = None
+        self.button_calc = None
+        self.button_layout = None
+        self.diffex_output = None
+        self._which_to_select = "blue"
+        self._ignore_observe = False
+        import numpy as np
+        subset_p = np.min([1, self.n_point_to_display / self.loom.shape[1]])
+        self.subset_mask = np.random.choice([True, False],
+                                            p=[subset_p, 1 - subset_p],
+                                            size=loom.shape[1])
 
-    from ipywidgets import ToggleButtons, VBox, HTML, widgets
+    def _fig_setup(self):
+        """ """
 
-    lasso_sel = LassoSelector()
-    subset_mask = np.random.choice([True, False],
-                                   p=[0.05, 0.95],
-                                   size=loom.shape[1])
-    xs, ys = LinearScale(), LinearScale()
-    #data = np.arange(20)
-    from ipywidgets import FloatSlider, HBox, VBox, Button
+        import numpy as np
+        from bqplot import LinearScale, Scatter, Figure, Axis
+        from ipywidgets import HBox, VBox, Button, widgets
 
-    button_sel1 = Button(description='Selection 1')
-    button_sel2 = Button(description='Selection 2')
-    button_calc = Button(description='Calculate!')
-    button_layout = HBox([button_sel1, button_sel2, button_calc])
-    diffex_output = widgets.Output()
-    #diffex_output2 = widgets.Output()
+        self.xs, self.ys = LinearScale(), LinearScale()
+        self.button_sel0 = Button(description='Reset axes', icon='fa-refresh')
+        self.button_sel1 = Button(description='Zoom', icon='fa-arrows')
+        self.button_sel2 = Button(description='Select', icon='fa-crosshairs')
+        self.button_sel3 = Button(description='Resubset', icon='fa-refresh')
+        self.button_calc = Button(
+            description='Calculate!',
+            icon='fa-calculator',
+        )
+        self.button_layout = VBox([
+            HBox([self.button_sel0, self.button_sel1, self.button_sel2]),
+            HBox([self.button_sel3, self.button_calc])
+        ])
+        self.diffex_output = widgets.Output()
+        self.scatter = Scatter(x=loom.ca[self.subset_mask][self.coord1],
+                               y=loom.ca[self.subset_mask][self.coord2],
+                               scales={
+                                   "x": self.xs,
+                                   "y": self.ys
+                               },
+                               default_size=1,
+                               colors=['gray'])
+        xax, yax = Axis(scale=self.xs,
+                        label="UMAP 1"), Axis(scale=self.ys,
+                                              label="UMAP 2",
+                                              orientation="vertical")
+        self.fig = Figure(
+            marks=[
+                self.scatter,
+            ],
+            axes=[xax, yax],
+            title="Panopticopter",
+            #interaction=lasso_sel,
+        )
 
-    scatter_lasso = Scatter(
-        x=loom.ca[coord1][subset_mask],
-        y=loom.ca[coord2][subset_mask],
-        scales={
-            "x": xs,
-            "y": ys
-        },
-        default_size=1,
-        colors=['gray'])
-    #scatter_lasso = Scatter(x=data, y=data, scales={"x": xs, "y": ys}, colors=["skyblue"])
-    #bar_lasso = Bars(x=data, y=data / 2.0, scales={"x": xs, "y": ys})
-    xax_lasso, yax_lasso = Axis(scale=xs,
-                                label="UMAP 1"), Axis(scale=ys,
-                                                      label="UMAP 2",
-                                                      orientation="vertical")
-    fig_lasso = Figure(
-        marks=[
-            scatter_lasso,
-        ],
-        axes=[xax_lasso, yax_lasso],
-        title="Panopticopter",
-        interaction=lasso_sel,
-    )
-    lasso_sel.marks = [
-        scatter_lasso,
-    ]
-    #fig_lasso.marks.set_metadata(1)
-    selection1 = None
-    selection2 = None
+    def _zoom_setup(self):
+        """ """
+        from bqplot.interacts import (PanZoom)
+        panzoom = PanZoom(scales={"x": [self.xs], "y": [self.ys]})
+        self.fig.interaction = panzoom
 
-    def recolor(selection1, selection2):
-        colors = []
-        if selection1 is None:
-            selection1 = []
-        if selection2 is None:
-            selection2 = []
-        for i in range(subset_mask.sum()):
-            if i in selection1:
-                colors.append('blue')
-            elif i in selection2:
-                colors.append('red')
+    def _reset(self):
+        """ """
+        import numpy as np
+        xmin = float(np.min(self.loom.ca[self.coord1]))
+        xmax = float(np.max(self.loom.ca[self.coord2]))
+        ymin = float(np.min(self.loom.ca[self.coord1]))
+        ymax = float(np.max(self.loom.ca[self.coord2]))
+        self.scatter.scales['x'].min = xmin
+        self.scatter.scales['x'].max = xmax
+        self.scatter.scales['y'].min = ymin
+        self.scatter.scales['y'].max = ymax
+
+    def resubset(self):
+        """ """
+        import numpy as np
+        mask = np.array([True] * self.loom.shape[1])
+        if self.xs.min is not None:
+            mask *= self.loom.ca[self.coord1] >= self.xs.min
+        if self.xs.max is not None:
+            mask *= self.loom.ca[self.coord1] <= self.xs.max
+        if self.ys.min is not None:
+            mask *= self.loom.ca[self.coord2] >= self.ys.min
+        if self.ys.max is not None:
+            mask *= self.loom.ca[self.coord2] <= self.ys.max
+        subset_p = np.min([1, self.n_point_to_display / mask.sum()])
+        self.subset_mask = mask * np.random.choice(
+            [True, False], p=[subset_p, 1 - subset_p], size=loom.shape[1])
+        self.scatter.x = loom.ca[self.subset_mask][self.coord1]
+        self.scatter.y = loom.ca[self.subset_mask][self.coord2]
+
+    def _lasso_setup(self):
+        """ """
+        if self.fig.interaction is not None:
+            self.fig.interaction.close()
+            self.scatter.colors = ['gray']  #*self.subset_mask.sum()
+            self.selection1 = []
+            self.selection2 = []
+
+        from bqplot.interacts import (
+            LassoSelector,
+            #PanZoom,
+        )
+        lasso_sel = LassoSelector()
+        lasso_sel.marks = [
+            self.scatter,
+        ]
+        self.fig.interaction = lasso_sel
+        self.fig.interaction.color = 'black'
+
+        # _which_to_select = self._which_to_select
+        def make_selection():
+            """ """
+            self.scatter.unobserve_all()
+            if 'blue' not in self.scatter.colors:
+                which_to_select = 'blue'
+            elif 'red' not in self.scatter.colors:
+                which_to_select = 'red'
             else:
-                colors.append('gray')
-        #global scatter_lasso
+                which_to_select = None
+            if which_to_select == "blue":
 
-        scatter_lasso.colors = colors
-        #lasso_sel.marks = [scatter_lasso, ]
+                self.selection1 = self.scatter.selected
+                self._ignore_observe = True
+                lasso_sel.reset()
+                self._ignore_observe = False
 
-    def button1_click(widget):
-        global selection1
-        global selection2
-        selection1 = scatter_lasso.selected
-        lasso_sel.reset()
-        recolor(selection1, selection2)
+                #recolor()
+            elif which_to_select == "red":
+                self.selection2 = self.scatter.selected
+                self._ignore_observe = True
+                lasso_sel.reset()
+                self._ignore_observe = False
 
-    def button2_click(widget):
-        global selection1
-        global selection2
-        selection2 = scatter_lasso.selected
-        lasso_sel.reset()
-        recolor(selection1, selection2)
+                #recolor()
+            self.scatter.observe(select_and_recolor, names=['selected'])
 
-    def button_calc_click(widget):
-        diffex_output.clear_output()
-        from panopticon.analysis import get_cluster_differential_expression
-        mask1 = np.isin(loom.ca['cellname'],
-                        loom.ca['cellname'][subset_mask][selection1])
-        mask2 = np.isin(loom.ca['cellname'],
-                        loom.ca['cellname'][subset_mask][selection2])
-        with diffex_output:
+        def select_and_recolor(self):
+            """ """
+            make_selection()
+            recolor()
 
-            if mask1 is not None and mask2 is not None:
-                diffex = get_cluster_differential_expression(loom,
-                                                            layername,
-                                                             mask1=mask1,
-                                                             mask2=mask2,
-                                                             verbose=True)
-            interesting_columns = [
-                'pvalue', 'CommonLanguageEffectSize', 'GeneAlternateName'
-            ]
-            diffex = diffex[interesting_columns]
-            diffex.columns = ['p', 'CLES', 'gene']
-            print('UPregulated in RED')
-            display(diffex.sort_values('CLES').head(10))
-            print('UPregulated in BLUE')
-            display(diffex.sort_values('CLES', ascending=False).head(10))
+        def recolor():
+            """ """
+            self.scatter.unobserve_all()
+            if self.selection1 is None:
+                self.selection1 = []
+            if self.selection2 is None:
+                self.selection2 = []
+            colors = []
+            for i in range(self.subset_mask.sum()):
+                if i in self.selection1:
+                    colors.append('blue')
+                elif i in self.selection2:
+                    colors.append('red')
+                else:
+                    colors.append('gray')
+            self.scatter.colors = colors
+            self.scatter.observe(select_and_recolor, names=['selected'])
 
-    #  diffex_output1.append_display_data(HTML(diffex.sort_values('CommonLanguageEffectSize').head(10).to_html()))
-    #with diffex_output2:
-    #  diffex_output2.append_display_data(HTML(diffex.sort_values('CommonLanguageEffectSize', ascending=False).head(10).to_html()))
+        self.scatter.unobserve_all()
+        self.scatter.observe(select_and_recolor, names=['selected'])
 
-    button_sel1.on_click(button1_click, )
-    button_sel2.on_click(button2_click)
-    button_calc.on_click(button_calc_click)
+    def __call__(self):
+        import numpy as np
+        from bqplot import pyplot as plt
+        #from bqplot import DateScale, LinearScale, Axis, Lines, Scatter, Bars, Hist, Figure
+        from bqplot import LinearScale, Scatter, Figure, Axis
+        from ipywidgets import HBox, VBox, Button, widgets
 
-    from IPython.display import display
-    #scatter_lasso.enable_move = True
-    #fig_lasso
-    final_layout = HBox([VBox([fig_lasso, button_layout]), diffex_output])
-    return final_layout
+        self._fig_setup()
+
+        #  lasso_sel.o
+        def button0_click(widget):
+            """
+
+            Parameters
+            ----------
+            widget :
+                
+
+            Returns
+            -------
+
+            """
+            self._reset()
+
+        def button1_click(widget):
+            """
+
+            Parameters
+            ----------
+            widget :
+                
+
+            Returns
+            -------
+
+            """
+            self._zoom_setup()
+
+        def button2_click(widget):
+            """
+
+            Parameters
+            ----------
+            widget :
+                
+
+            Returns
+            -------
+
+            """
+            self._lasso_setup()
+
+        def button3_click(widget):
+            """
+
+            Parameters
+            ----------
+            widget :
+                
+
+            Returns
+            -------
+
+            """
+            self.resubset()
+
+        def button_calc_click(widget):
+            """
+
+            Parameters
+            ----------
+            widget :
+                
+
+            Returns
+            -------
+
+            """
+            self.diffex_output.clear_output()
+            from panopticon.analysis import get_cluster_differential_expression
+            mask1 = np.isin(
+                loom.ca['cellname'],
+                loom.ca['cellname'][self.subset_mask][self.selection1])
+            mask2 = np.isin(
+                loom.ca['cellname'],
+                loom.ca['cellname'][self.subset_mask][self.selection2])
+            with self.diffex_output:
+
+                if mask1 is not None and mask2 is not None:
+                    diffex = get_cluster_differential_expression(
+                        self.loom,
+                        self.layername,
+                        mask1=mask1,
+                        mask2=mask2,
+                        verbose=True)
+                interesting_columns = [
+                    'pvalue', 'CommonLanguageEffectSize', 'GeneAlternateName'
+                ]
+                diffex = diffex[interesting_columns]
+                diffex.columns = ['p', 'CLES', 'gene']
+                d1 = diffex.sort_values('CLES').head(10)
+                d2 = diffex.sort_values('CLES', ascending=False).head(10)
+                print('UPregulated in RED')
+                display(diffex.sort_values('CLES').head(10), exclude='index')
+                print('UPregulated in BLUE')
+                display(diffex.sort_values('CLES', ascending=False).head(10))
+
+        #  diffex_output1.append_display_data(HTML(diffex.sort_values('CommonLanguageEffectSize').head(10).to_html()))
+        #with diffex_output2:
+        #  diffex_output2.append_display_data(HTML(diffex.sort_values('CommonLanguageEffectSize', ascending=False).head(10).to_html()))
+        self.button_sel0.on_click(button0_click)
+        self.button_sel1.on_click(button1_click, )
+        self.button_sel2.on_click(button2_click)
+        self.button_sel3.on_click(button3_click)
+        self.button_calc.on_click(button_calc_click)
+
+        from IPython.display import display
+        #scatter_lasso.enable_move = True
+        #fig_lasso
+        final_layout = HBox(
+            [VBox([self.fig, self.button_layout]), self.diffex_output])
+        return final_layout
