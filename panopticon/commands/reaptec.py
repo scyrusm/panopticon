ff8e88430dc3256ab1eb02eb82d3471fc49af9ac
Samuel Markson <smarkson@alum.mit.edu>
2024-08-09 09:51:44 -0400

    syntax fix

--- panopticon/commands/reaptec.py
+++ panopticon/commands/reaptec.py
@@ -4,14 +4,14 @@ import os
 
 def reaptec_main(fastq_dir, cellranger_output_dir, star_reference_dir):
     # create whitelist
-    command = "{0}/outs/filtered_feature_bc_matrix/barcodes.tsv.gz | sed -e 's/-1//g' > {0}/outs/filtered_feature_bc_matrix/barcode_whitelist.txt".format(
+    command = "zcat {0}/outs/filtered_feature_bc_matrix/barcodes.tsv.gz | sed -e 's/-1//g' > {0}/outs/filtered_feature_bc_matrix/barcode_whitelist.txt".format(
         cellranger_output_dir)
     os.system(command)
 
     # perform whitelisted star mapping
     for fastq in [x for x in os.listdir(fastq_dir) if 'R1' in x]:
-        command = "STAR --runThreadN 32 --genomeDIR {} --readFilesIn {} {} --solocCBwhitelist {} --soloBarcodeMate 1 --clip5bNbases 39 0 ".format(
-            star_reference_dir, fastq, fastq.replace('R1', 'R2'),
+        command = "STAR --runThreadN 32 --genomeDir {0} --readFilesIn {1}/{2} {1}/{3} --soloCBwhitelist {4} --soloBarcodeMate 1 --clip5pNbases 39 0 ".format(
+            star_reference_dir, fastq_dir,fastq, fastq.replace('R1', 'R2'),
             cellranger_output_dir +
             '/outs/filtered_feature_bc_matrix/barcode_whitelist.txt')
         command += "--readFilesCommand zcat --soloType CB_UMI_Simple --soloCBstart 1 --soloCBlen 16 --soloUMIstart 17 --soloUMIlen 10 --soloStrand Reverse --outFileNamePrefix {}".format(
